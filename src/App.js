91cbc568b1d90c26263573f74f0ecaeef10cadf5
student <student@L20-03>
2026-03-10 19:18:17 +0200

    added jwt tokens

--- src/App.js
+++ src/App.js
@@ -1,345 +1,193 @@
-//import logo from './logo.svg';
 import { useState,  useEffect } from 'react';
 import './App.css';
+import { replace, Route, Routes, useLocation, useNavigate } from 'react-router';
 
-// function GetInfo({element}){
-//   return(
-//       <><tr>
-//         <td>{element.id}</td>
-//         <td>{element.name}</td>
-//         <td>{element.surname}</td>
-//         <td>{element.premium}</td>
-//         <td>{element.salary}</td>
-//         <td>{element.departmentId}</td>
-//         </tr>
-//       </>
-//     )
-// }
 
-// function EditPage({callback, Cpage, pagesCount, totalCount, itemsOnPage}){
+// document.addEventListener('contextmenu', (event) => {
+//     event.preventDefault(); // Запрещает появление браузерной панели
+// });
 
-//   //const [buttons, setButtons] = useState("");
-  
-//   const buttons = [];
 
-//   if(pagesCount <= 5){
-//     for (let i = 1; i <= Math.min(pagesCount, 5); i++) {
-//       buttons.push(
-//         <button key={i} onClick={() => callback(i)} className={i == Cpage ? 'btn-choose-page active' : 'btn-choose-page' }>
-//           {i}
-//         </button>
-//       );
+// function GetMap({map, callBackFetch, islose, flagsMap, setFlagsMap}){
+//   const mapHtml = []
+//   //let flagsMap = flagsMaps
+//   for(let i = 0; i < 10; i++){
+//     let items = []
+//     for(let j = 0; j < 10; j++){
+//       items.push(<td 
+//         className={
+//           map[i][j] === "B" ? "bomb" : 
+//           map[i][j] === "O" ? "clear" :
+//           map[i][j] === "1" ? "numb1" :
+//           map[i][j] === "2" ? "numb2" :
+//           map[i][j] === "3" ? "numb3" :
+//           map[i][j] === "4" ? "numb4" :
+//           map[i][j] === "5" ? "numb5" :
+//           map[i][j] === "6" ? "numb6" :
+//           map[i][j] === "7" ? "numb7" :
+//           map[i][j] === "8" ? "numb8" :
+//           flagsMap[i][j] === "F" ? "flag" : "closen"
+//         }
+//         onMouseDown={
+//           map[i][j] === "" ? (e) => 
+//             {
+//               //console.log(e.button)
+//               if(e.button === 0){
+//                 if(flagsMap[i][j] !== "F"){
+//                   callBackFetch(j, i)
+//                   }
+//               }
+//               else if (e.button === 2){
+//                 console.log(e.button)
+//                 //if(flagsMap[i][j] !== "F")
+//                   flagsMap[i][j] = "F";
+//                 // else
+//                 //   flagsMap[i][j] = "";            
+//               }
+//             }
+//            : undefined}
+//         >{map[i][j] !== "B" ? map[i][j] !== "O" ? map[i][j] : "" : "" }</td>)
 //     }
+//     mapHtml.push(<tr>{items}</tr>)
 //   }
-//   else if (Cpage <= 3) {
-//     for (let i = 1; i <= Math.min(pagesCount, 4); i++) {
-//       buttons.push(
-//         <button key={i} onClick={() => callback(i)} className={i == Cpage ? 'btn-choose-page active' : 'btn-choose-page' }>
-//           {i}
-//         </button>
-//       );
-//     }
-//     if(pagesCount - Cpage >= 2){
-//       if(pagesCount - Cpage >= 3){
-//         buttons.push(
-//           <span className='btn-choose-page'>
-//             ...
-//           </span>
-//         );
-//       }
-//       buttons.push(
-//         <button key={pagesCount} onClick={() => callback(pagesCount)} className={pagesCount == Cpage ? 'btn-choose-page active' : 'btn-choose-page' }>
-//           {pagesCount}
-//         </button>
-//       );
-      
-//     }
-//   } else {
-//     if(Cpage >= 4){
-//     buttons.push(
-//         <button key={1} onClick={() => callback(1)} className={1 == Cpage ? 'btn-choose-page active' : 'btn-choose-page' }>
-//           1
-//         </button>
-//       );
-//       buttons.push(
-//         <span className='btn-choose-page'>
-//           ...
-//         </span>
-//       );
-//     }
-//     for (let i = Cpage - 1; i <= Math.min(pagesCount, Cpage + 1); i++) {
-//       buttons.push(
-//         <button key={i} onClick={() => callback(i)} className={i == Cpage ? 'btn-choose-page active' : 'btn-choose-page' }>
-//           {i}
-//         </button>
-//       );
-//     }
-
-//     if(pagesCount - Cpage >= 2){
-//       if(pagesCount - Cpage >= 3){
-//         buttons.push(
-//           <span className='btn-choose-page'>
-//             ...
-//           </span>
-//         );
-//       }
-//       buttons.push(
-//         <button key={pagesCount} onClick={() => callback(pagesCount)} className={pagesCount == Cpage ? 'btn-choose-page active' : 'btn-choose-page' }>
-//           {pagesCount}
-//         </button>
-//       );
-      
-//     }
-
-//   }
-
-//   return (
-//     <div className='btn-row-gen'>
-//       <div className='btn-row'>
-//         <button onClick={() => {callback(Cpage - 1)}} className='btn-choose-page'>←</button>
-//         {/* <span>{Cpage} / {pagesCount}</span> */}
-//           {buttons}
-//         <button onClick={() => {callback(Cpage + 1)}} className='btn-choose-page'>→</button>
-//       </div>
-//       <div className='right-row'>
-//         {Math.max(itemsOnPage * (Cpage - 1))} - {Math.min(itemsOnPage * Cpage, totalCount)} from {totalCount}
-//       </div>
-//     </div>
-//   )
+//   return (mapHtml)
 // }
-
-
 // function App() {
-
-//   const itemsOnPage = 2;
-//   const [Gpage, setPage] = useState(1);
-//   const [htmldata, setHtmlData] = useState();
-//   const [state, setState] = useState(1);
-//   const [formData, setFormData] = useState({
-//     id: 0,
-//     name: "",
-//     surname: "",
-//     premium: "",
-//     salary: "",
-//     departmentId: "",
-//   });
-
-//   const [totalCount, setTotalCount] = useState(0);
-//   const [pagesCount, setPagesCount] = useState(0);
-
-
-//   // https://localhost:7198/Doctors/doctors
-
-//   async function func(page = Gpage){
-//     let a = await fetch(`https://localhost:7198/Doctors/paged/${page}?size=${itemsOnPage}`)
+//   const [id, setId] = useState()
+//   const [map, setMap] = useState()
+//   let flagsMap = Array(10).fill(null).map(() => Array(10).fill(""))
+//   const [iswin, setIswin] = useState(false)
+//   const [islose, setIslose] = useState(false)
+//   async function func(){
+//     let a = await fetch(`https://localhost:7198/FieldsControler/fields/${id}`)
 //     let b = await a.json()
-//     //console.log(b)
-//     if(b !== null && b !== undefined){
-//       //setData(b)
-
-//       setTotalCount(b.totalCount)
-//       setPagesCount(b.pagesCount)
-//       setHtmlData("")
-//       b.items.forEach(e =>{
-//         setHtmlData((prev) =>([...prev, <GetInfo element={e}/>]))
-        
-//       })
-//     }
-
+//     console.log(b)
+//     setMap(JSON.parse(b.userFieldJson))
+//     // if(b !== null && b !== undefined){
+//     //   //setData(b)
+//     //   setTotalCount(b.totalCount)
+//     //   setPagesCount(b.pagesCount)
+//     //   setHtmlData("")
+//     //   b.items.forEach(e =>{
+//     //     setHtmlData((prev) =>([...prev, <GetInfo element={e}/>]))        
+//     //   })
+//     // }
 //   }
-//   useEffect(() => {func()}, []);
-
-//   function handleChange(e) {
-//     const { name, value } = e.target;
-//     setFormData((prev) => ({
-//       ...prev,
-//       [name]: value,
-//     }));
+//   async function fetchMove(x, y){
+//     let a = await fetch(`https://localhost:7198/FieldsControler/move?id=${id}&x=${x}&y=${y}`, {method: "Post"})
+//     let b = await a.json()
+//     console.log(b)
+//     setMap(b.field)
+//     setIslose(b.isLose)
+//     setIswin(b.isWin)
 //   }
-
-//   function AddDoctor(){
-//     fetch("https://localhost:7198/Doctors/AddDoctors",{
-//       method: 'POST',
-//       headers: { 'Content-Type': 'application/json' },
-//       body: JSON.stringify(formData)
-//     } )
+//   async function createNewField(sizeX = 10, sizeY = 10, mines = 10){
+//     let a = await fetch(`https://localhost:7198/FieldsControler/CreateField?SizeX=${sizeX}&SizeY=${sizeY}&Mines=${mines}`, {method: "Post"})
+//     let b = await a.json()
+//     console.log(b)
+//     setId(b.id)
+//     setMap(JSON.parse(b.userFieldJson))
+//     setIslose(false)
+//     setIswin(false)
+//     flagsMap = Array(10).fill(null).map(() => Array(10).fill(""))
 //   }
-
-//   async function UpdatePage(newpage){
-//     if(newpage > 0 && newpage <= pagesCount){
-//       await setPage(newpage)
-//       // console.log(newpage)
-//       func(newpage)
-//     }
-//   }
-
-//   return (
-//     <>
-//     <button onClick={() => setState(1)}>Doctors</button>
-//     <button onClick={() => setState(2)}>Add doctor</button>
-//     {state === 1 && htmldata != null &&
-//     <>
-//     <table>
-//       <thead>
-//         <tr>
-//           <td>ID</td>
-//           <td>Name</td>
-//           <td>Surname</td>
-//           <td>Premium</td>
-//           <td>Salary</td>
-//           <td>DepartmentId</td>
-//         </tr>
-//       </thead>
+//   useEffect(() => {
+//   id !== undefined && func();
+//   }, []);
+//   return (<>
+//   <button onClick={() => createNewField()}>Create new Field</button>
+//   <input name="Id" placeholder='Id' value={id} onChange={(e) => {setId(e.target.value)}}></input>
+//   <button onClick={() => func()}>Find</button>
+//     {map != null && <table>
 //       <tbody>
-//         {htmldata}
-
+//       <GetMap map={map} islose={islose} flagsMap = {flagsMap} setFlagsMap = {(a) => flagsMap = a} callBackFetch={(x, y) => fetchMove(x, y)}/>
 //         </tbody>
-//     </table>
-//     <EditPage callback= {(e)=> UpdatePage(e)} Cpage={Gpage} pagesCount={pagesCount} totalCount={totalCount} itemsOnPage={itemsOnPage}/>
-    
-//     </>
-//     }
-//     { state === 2 && 
-//     <>
-//     <form onSubmit={(e) => {
-//       e.preventDefault();
-//       AddDoctor();
-
-//       setTimeout(() => func(), 2000);
-//     }}>
-//       <input required name='name' value={formData.name} onChange={handleChange} placeholder='Name'></input>
-//       <input required name='surname' value={formData.surname} onChange={handleChange} placeholder='Surname'></input>
-//       <input required type='number' name='premium' value={formData.premium} onChange={handleChange} placeholder='Premium'></input>
-//       <input required type='number' name='salary' value={formData.salary} onChange={handleChange} placeholder='Salary'></input>
-//       <input required name='departmentId' value={formData.departmentId} onChange={handleChange} placeholder='DepartmentId'></input>
-//       <button type='submit'>Add</button>
-//     </form>
-//     </>
-
-//     }
-//     </>
-//   );
+//     </table> }
+//   {islose && <h1>You Lose!</h1>}      
+//   {iswin && <h1>You Win!</h1>}
+//   </>)
 // }
-
-document.addEventListener('contextmenu', (event) => {
-    event.preventDefault(); // Запрещает появление браузерной панели
-});
+// export default App;
 
 
-function GetMap({map, callBackFetch, islose, flagsMap, setFlagsMap}){
-  const mapHtml = []
-  //let flagsMap = flagsMaps
 
 
-  for(let i = 0; i < 10; i++){
-    let items = []
-    for(let j = 0; j < 10; j++){
-      items.push(<td 
-        className={
-          map[i][j] === "B" ? "bomb" : 
-          map[i][j] === "O" ? "clear" :
-          map[i][j] === "1" ? "numb1" :
-          map[i][j] === "2" ? "numb2" :
-          map[i][j] === "3" ? "numb3" :
-          map[i][j] === "4" ? "numb4" :
-          map[i][j] === "5" ? "numb5" :
-          map[i][j] === "6" ? "numb6" :
-          map[i][j] === "7" ? "numb7" :
-          map[i][j] === "8" ? "numb8" :
-          flagsMap[i][j] === "F" ? "flag" : "closen"
-
-        }
-        onMouseDown={
-          map[i][j] === "" ? (e) => 
-            {
-              //console.log(e.button)
-              if(e.button === 0){
-                if(flagsMap[i][j] !== "F"){
-                  callBackFetch(j, i)
-                  }
-              }
-              else if (e.button === 2){
-                console.log(e.button)
-                //if(flagsMap[i][j] !== "F")
-                  flagsMap[i][j] = "F";
-                // else
-                //   flagsMap[i][j] = "";
-                
-              }
-            }
-           : undefined}
-        >{map[i][j] !== "B" ? map[i][j] !== "O" ? map[i][j] : "" : "" }</td>)
+function LoginPage(){
+  
+  const navigate = useNavigate()
+  useEffect(()=>{
+    if (localStorage.getItem("token") != null){
+      navigate("/", {replace:true})
     }
-    mapHtml.push(<tr>{items}</tr>)
+
+  })
+
+  function login(e){
+    e.preventDefault()
+
+    let credentials = {
+      "email" : document.getElementById("userEmail").value,
+      "password" : document.getElementById("userPassword").value,
+    }
+    fetch("https://localhost:7198/Users/login", {
+      method: "POST",
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(credentials)
+    }).then(response => response.text())
+    .then(data => {
+      localStorage.setItem("token", data)
+      navigate("/", {replace:true})
+    })
+    .catch(err => {
+      console.log(err)
+    })
+
   }
 
-  return (mapHtml)
+  return(
+    <>
+    <form onSubmit={(e) => login(e)}>
+      <input id="userEmail" type='email' placeholder='email'/>
+      <input id="userPassword" type='password' placeholder='password'/>
+      <button type='submit'>Submit</button>
+    </form>
+    </>
+  )
 }
+function IndexPage(){
+
+  const navigate = useNavigate()
+  useEffect(()=>{
+    if (localStorage.getItem("token") === null){
+      navigate("/login", {replace:true})
+    }
+
+  })
+  return(
+    <>
+    <h1>Index</h1>
+    </>
+  )
+}
+
 
 function App() {
-  const [id, setId] = useState()
-  const [map, setMap] = useState()
-  let flagsMap = Array(10).fill(null).map(() => Array(10).fill(""))
-  const [iswin, setIswin] = useState(false)
-  const [islose, setIslose] = useState(false)
-  async function func(){
-    let a = await fetch(`https://localhost:7198/FieldsControler/fields/${id}`)
-    let b = await a.json()
-    console.log(b)
-    setMap(JSON.parse(b.userFieldJson))
-    
-    // if(b !== null && b !== undefined){
-    //   //setData(b)
 
-    //   setTotalCount(b.totalCount)
-    //   setPagesCount(b.pagesCount)
-    //   setHtmlData("")
-    //   b.items.forEach(e =>{
-    //     setHtmlData((prev) =>([...prev, <GetInfo element={e}/>]))
-        
-    //   })
-    // }
-
-  }
-  async function fetchMove(x, y){
-    let a = await fetch(`https://localhost:7198/FieldsControler/move?id=${id}&x=${x}&y=${y}`, {method: "Post"})
-    let b = await a.json()
-    console.log(b)
-    setMap(b.field)
-    setIslose(b.isLose)
-    setIswin(b.isWin)
-
+  function function1(){
+  fetch("https://localhost:7198/Users").then(data => data.json()).then(text => console.log(text))
   }
 
-  async function createNewField(sizeX = 10, sizeY = 10, mines = 10){
-    let a = await fetch(`https://localhost:7198/FieldsControler/CreateField?SizeX=${sizeX}&SizeY=${sizeY}&Mines=${mines}`, {method: "Post"})
-    let b = await a.json()
-    console.log(b)
-    setId(b.id)
-    setMap(JSON.parse(b.userFieldJson))
-    setIslose(false)
-    setIswin(false)
-    flagsMap = Array(10).fill(null).map(() => Array(10).fill(""))
-  }
-
-  useEffect(() => {
-  id !== undefined && func();
-  }, []);
-
-  return (<>
-  <button onClick={() => createNewField()}>Create new Field</button>
-  <input name="Id" placeholder='Id' value={id} onChange={(e) => {setId(e.target.value)}}></input>
-  <button onClick={() => func()}>Find</button>
-    {map != null && <table>
-      <tbody>
-      <GetMap map={map} islose={islose} flagsMap = {flagsMap} setFlagsMap = {(a) => flagsMap = a} callBackFetch={(x, y) => fetchMove(x, y)}/>
-        </tbody>
-    </table> }
-  {islose && <h1>You Lose!</h1>}
-      
-  {iswin && <h1>You Win!</h1>}
-  </>)
+  return(
+    <>
+    <Routes>
+      <Route path='/' element={<IndexPage />} />
+      <Route path='/login' element={<LoginPage />} />
+    </Routes>
+    <button onClick={() => function1()}>Click me!</button>
+    </>
+  )
 }
-
 
 export default App;
